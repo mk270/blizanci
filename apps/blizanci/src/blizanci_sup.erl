f7eae5915327d35b184298db175fff3c9429f03a
Martin Keegan <martin@no.ucant.org>
2020-05-19 19:37:11 +0100

    fix copyright boilerplate

--- apps/blizanci/src/blizanci_sup.erl
+++ apps/blizanci/src/blizanci_sup.erl
@@ -1,11 +1,10 @@
-%%%-------------------------------------------------------------------
-%%% @author Martin Keegan <mk270@savile>
-%%% @copyright (C) 2020, Martin Keegan
-%%% @doc
-%%%
-%%% @end
-%%% Created : 19 May 2020 by Martin Keegan <mk270@savile>
-%%%-------------------------------------------------------------------
+%% Blizanci, a Gemini protocol server, by Martin Keegan
+%%
+%% To the extent (if any) permissible by law, Copyright (C) 2020  Martin Keegan
+%%
+%% This programme is free software; you may redistribute and/or modify it under
+%% the terms of the Apache Software Licence v2.0.
+
 -module(blizanci_sup).
 
 -behaviour(supervisor).
