b5a38bb4c02659fc3bab69a9123ba41901153f6b
Martin Keegan <martin@no.ucant.org>
2022-06-11 03:49:07 +0100

    add todo note

--- apps/blizanci/src/blizanci_servlet_container.erl
+++ apps/blizanci/src/blizanci_servlet_container.erl
@@ -125,6 +125,7 @@ defer_request(Module, Matches, Req, ServerConfig, RouteOpts) ->
     Args = [self(), Module, Matches, Req, ServerConfig, RouteOpts],
     process_flag(trap_exit, true),
     %% TBD: set a timeout
+    %% TBD: there should also be a timeout not on startup but on completion
     case proc_lib:start_link(?MODULE, init, [Args]) of
         {ok, Pid} -> {init_servlet, Pid};
         {error, _} -> {error_code, internal_server_error}
