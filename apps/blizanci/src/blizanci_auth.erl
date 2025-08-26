ae821889c04a697a9b02e175fc9983ccfacf2896
Martin Keegan <martin@no.ucant.org>
2025-08-26 22:49:00 +0100

    try to improve specs for auth

--- apps/blizanci/src/blizanci_auth.erl
+++ apps/blizanci/src/blizanci_auth.erl
@@ -10,43 +10,56 @@
 % restricted: certificate must be presented
 % public: no certificate requirement
 
-
 -module(blizanci_auth).
 -include("blizanci_types.hrl").
 
 -export([authorisation_policy/1, authorised/2, valid_authz_policy/1]).
 
--spec authorisation_policy(map()) ->
-          {'ok', authorisation()} | {'error', atom()}.
 %% @doc Derive the authorisation policy of a given routing table entry
 %% @returns {ok, public | restricted | private} | {error, atom()}
 %% @end
+-spec authorisation_policy(Options) -> Result
+              when Options :: map(),
+                   Result  :: {'ok', authorisation()} | {'error', atom()}.
+
 authorisation_policy(Options) when is_map(Options) ->
     #{ authorisation := Auth } = Options,
     valid_authz_policy(Auth).
 
--spec valid_authz_policy(term()) -> {ok, authorisation()} | {error, atom()}.
+
 %% @doc Check if the argument represents a valid authorisation policy
 %% @end
+-spec valid_authz_policy(Policy) -> Result
+              when Policy :: term(),
+                   Result :: {ok, authorisation()} | {error, atom()}.
+
 valid_authz_policy(public) -> {ok, public};
 valid_authz_policy(restricted) -> {ok, restricted};
 valid_authz_policy({private, Keys}) -> {ok, {private, Keys}};
 valid_authz_policy(_) -> {error, invalid_authz_policy}.
 
--spec authorised(AuthPolicy::authorisation(), Request::map()) ->
-          'authorised' | {'error_code', atom()}.
+
 %% @doc Check if a route's authorisation policy permits a given request
 %% @param AuthPolicy the authorisation policy associated with the route
 %% @param Request the Gemini request
 %% @end
+-spec authorised(AuthPolicy, Request) -> Result
+              when AuthPolicy :: authorisation(),
+                   Request    :: map(),
+                   Result     :: 'authorised' | {'error_code', atom()}.
+
 authorised(public, _Request) -> authorised;
 authorised(AuthPolicy, Request) ->
     #{ client_cert := Cert } = Request,
     cert_authorised(AuthPolicy, Cert).
 
+
 %% TODO: should check for expiry
--spec cert_authorised(authorisation(), term()) ->
-          'authorised' | {'error_code', atom()}.
+-spec cert_authorised(Authorisation, CertInfo) -> Result
+              when Authorisation :: authorisation(),
+                   CertInfo      :: term(),
+                   Result        :: 'authorised' | {'error_code', atom()}.
+
 cert_authorised(_, {error, no_peercert}) ->
     {error_code, cert_required};
 cert_authorised(restricted, {ok, _AnyCert}) ->
@@ -59,8 +72,11 @@ cert_authorised({private, Certs}, {ok, Cert}) when is_map(Cert) ->
     end.
 
 
--spec cert_issued_by_any(map(), [string()]) ->
-          {'ok', string()} | 'fail'.
+-spec cert_issued_by_any(Cert, Issuers) -> Result
+              when Cert    :: map(),
+                   Issuers :: [string()],
+                   Result  :: {'ok', string()} | 'fail'.
+
 cert_issued_by_any(Cert, []) when is_map(Cert) -> fail;
 cert_issued_by_any(Cert, [Issuer|Tail]) when is_list(Issuer) and
                                              is_map(Cert) ->
@@ -69,11 +85,16 @@ cert_issued_by_any(Cert, [Issuer|Tail]) when is_list(Issuer) and
         not_issuer -> cert_issued_by_any(Cert, Tail)
     end.
 
--spec cert_issued_by(map(), Issuer::string()) ->
-          'ok' | 'not_issuer'.
+
+-spec cert_issued_by(Cert, Issuer) -> Result
+              when Cert   :: map(),
+                   Issuer :: string(),
+                   Result :: 'ok' | 'not_issuer'.
+
 cert_issued_by(Cert, Issuer) when is_map(Cert) ->
     IssuerCert = blizanci_x509:certificate_from_file(Issuer),
-    case public_key:pkix_is_issuer(Cert, IssuerCert) of
+    IsIssuer   = public_key:pkix_is_issuer(Cert, IssuerCert),
+    case IsIssuer of
         false -> not_issuer;
         true -> ok
     end.
