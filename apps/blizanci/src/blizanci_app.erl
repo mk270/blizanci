026e0b4a07813fd50cc96b6e09967ab6bcbb8842
M Keegan <martin@no.ucant.org>
2026-09-03 11:07:44 +0100

    move ranch under supervisor

--- apps/blizanci/src/blizanci_app.erl
+++ apps/blizanci/src/blizanci_app.erl
@@ -11,34 +11,13 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, prep_stop/1]).
-
--define(LISTENER, blizanci_service_ssl).
-
-listen() ->
-    ok = application:ensure_started(mime_lookup),
-    ok = application:ensure_started(ranch),
-
-    Config = blizanci_config:make(),
-    #{
-      ssl_opts        := SSL_Opts,
-      proto_opts      := Proto_Opts,
-      active_servlets := Servlets
-     } = Config,
-
-    [ ok = Servlet:start() || Servlet <- Servlets ],
-
-    {ok, Listener} = ranch:start_listener(?LISTENER,
-                                          ranch_ssl, SSL_Opts,
-                                          blizanci_connection, Proto_Opts),
-    {ok, Listener}.
+-export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    {ok, _Listener} = listen(), % the _Listener is bound to the ranch pid
-    {ok, _Pid} = blizanci_sup:start_link().
-
-prep_stop(_State) ->
-    ok = ranch:stop_listener(?LISTENER).
+    ok = application:ensure_started(mime_lookup),
+    ok = application:ensure_started(ranch),
+    Config = blizanci_config:make(),
+    blizanci_sup:start_link(Config).
 
 stop(_State) ->
     ok.
--- apps/blizanci/src/blizanci_sup.erl
+++ apps/blizanci/src/blizanci_sup.erl
@@ -12,20 +12,31 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
+-define(LISTENER, blizanci_service_ssl).
 
-start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link(Config) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, Config).
 
-init([]) ->
+init(Config) ->
+    #{
+      ssl_opts        := SSL_Opts,
+      proto_opts      := Proto_Opts,
+      active_servlets := Servlets
+     } = Config,
+
+    [ ok = Servlet:start() || Servlet <- Servlets ],
+
     SupFlags = #{strategy => one_for_one,
                  intensity => 1,
                  period => 5},
 
-    % No children, but application requires a top-level supervisor.
-    {ok, {SupFlags, []}}.
+    RanchChildSpec = ranch:child_spec(?LISTENER, ranch_ssl, SSL_Opts,
+                                      blizanci_connection, Proto_Opts),
+
+    {ok, {SupFlags, [RanchChildSpec]}}.
